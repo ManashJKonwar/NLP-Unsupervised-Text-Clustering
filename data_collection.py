732f8925374ca3071dd2cf69167b23063123f8f0
bharath31793 <bharathchowdary.gogineni@gmail.com>
2023-01-18 18:09:44 +0000

    adding data

--- data_collection.py
+++ data_collection.py
@@ -1,4 +1,29 @@
-import pandas as pd 
-import numpy as np 
-import requests 
+import pandas as pd
+#import numpy as np 
+import gzip
 
+import requests
+URL = "http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Cell_Phones_and_Accessories_5.json.gz"
+response = requests.get(URL)
+open("reviews_Cell_Phones_and_Accessories_5.json.gz", "wb").write(response.content)
+
+
+
+
+def parse(path):
+    g = gzip.open(path, 'rb')
+    for l in g:
+        yield eval(l)
+
+def getDF(path):
+    i = 0
+    df = {}
+    for d in parse(path):
+        df[i] = d
+        i += 1
+    return pd.DataFrame.from_dict(df, orient='index')
+
+df = getDF('reviews_Cell_Phones_and_Accessories_5.json.gz')
+
+
+print(df.shape)
