0a733258638723433079c32bcf02864d79dbaa68
ManashJKonwar <rickykonwar@gmail.com>
2023-01-22 22:50:37 +0530

    Enhancement: Integrating_preprocessing_pipeline_with_entry_script
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- index.py
+++ index.py
@@ -8,23 +8,38 @@ __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
 
 import pandas as pd
+from tqdm import tqdm 
+tqdm.pandas()
 
 from src.clustering import *
+from src.text_preprocesser import *
 from utility.data_collection import *
 
 if __name__ == '__main__':
-    category="reviews_Cell_Phones_and_Accessories_5"
+    category = "reviews_Cell_Phones_and_Accessories_5"
     cluster_data = None
+    text_column = "reviewText"
+    sample_frac = 0.1
     
     if not os.path.exists(os.path.join("input", category.lower() + ".csv")):
         cluster_data = getDF(category=category)
         cluster_data.to_csv(os.path.join("input", category.lower() + ".csv"))
     else:
-        cluster_data = pd.read_csv(os.path.join("input", category.lower() + ".csv"))        
+        cluster_data = pd.read_csv(os.path.join("input", category.lower() + ".csv"))   
+        
+    # Removing nan rows based on review text
+    cluster_data = cluster_data[cluster_data[text_column].notna()].reset_index(drop=True)
     
+    # Sampling
+    cluster_data = cluster_data.sample(frac=sample_frac).reset_index(drop=True)
+    
+    # Preprocessing Pipeline
+    cluster_data["preprocessed_text"] = cluster_data[text_column].progress_apply(lambda x: preprocess_text(x))
+    
+    # Clustering Pipeline
     clustering_instance = Clustering(
                             cluster_algo='HDBSCAN',
                             cluster_data=cluster_data,
-                            text_column="reviewText"
+                            text_column=text_column
                         )
     clustering_instance._cluster_instance.run_cluster()
