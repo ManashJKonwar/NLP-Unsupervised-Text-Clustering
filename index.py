3bce73c58f945e1154f397b7f0c0f641334d1033
ManashJKonwar <rickykonwar@gmail.com>
2023-01-22 21:20:14 +0530

    Enhancement: Integrating_data_collection_with_entry_script
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- index.py
+++ index.py
@@ -7,10 +7,23 @@ __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
 
+import pandas as pd
+
 from src.clustering import *
+from utility.data_collection import *
 
 if __name__ == '__main__':
+    category="reviews_Cell_Phones_and_Accessories_5"
+    cluster_data = None
+    
+    if not os.path.exists(os.path.join("input", category.lower() + ".csv")):
+        cluster_data = getDF(category=category)
+        cluster_data.to_csv(os.path.join("input", category.lower() + ".csv"))
+    else:
+        cluster_data = pd.read_csv(os.path.join("input", category.lower() + ".csv"))        
+    
     clustering_instance = Clustering(
                             cluster_algo='HDBSCAN',
-                            cluster_data=None
+                            cluster_data=cluster_data,
+                            text_column=""
                         )
