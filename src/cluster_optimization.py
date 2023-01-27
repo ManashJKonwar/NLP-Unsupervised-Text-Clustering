f4c3b42ee39d881c98bae59c740f687e60612f91
ManashJKonwar <rickykonwar@gmail.com>
2023-01-27 18:17:21 +0530

    Enhancement: Adding_cluster_optimization_function_using_hyperopt
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- src/cluster_optimization.py
+++ src/cluster_optimization.py
@@ -6,3 +6,93 @@ __version__ = "1.0.1"
 __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
+
+import hdbscan
+import numpy as np
+from functools import partial
+from hyperopt import fmin, tpe, STATUS_OK, space_eval, Trials
+
+from src.text_dimension_reduction import reduce_dimensions
+
+def generate_clusters(message_embeddings,
+                      n_neighbors,
+                      n_components, 
+                      min_cluster_size,
+                      random_state = None):
+    """
+    Generate HDBSCAN cluster object after reducing embedding dimensionality with UMAP
+    """
+    
+    umap_embeddings = (
+                        reduce_dimensions(n_neighbors = n_neighbors, 
+                                        n_components = n_components, 
+                                        text_embeddings = message_embeddings)
+                    )
+
+    clusters = hdbscan.HDBSCAN(min_cluster_size = min_cluster_size,
+                               metric='euclidean', 
+                               cluster_selection_method='eom').fit(umap_embeddings)
+
+    return clusters, umap_embeddings
+
+def score_clusters(clusters, prob_threshold = 0.05):
+    """
+    Returns the label count and cost of a given cluster supplied from running hdbscan
+    """
+    
+    cluster_labels = clusters.labels_
+    label_count = len(np.unique(cluster_labels))
+    total_num = len(clusters.labels_)
+    cost = (np.count_nonzero(clusters.probabilities_ < prob_threshold)/total_num)
+    
+    return label_count, cost
+
+def objective(params, embeddings, label_lower, label_upper):
+    """
+    Objective function for hyperopt to minimize, which incorporates constraints
+    on the number of clusters we want to identify
+    """
+    
+    clusters, reduced_embeddings = generate_clusters(embeddings, 
+                                                     n_neighbors = params['n_neighbors'], 
+                                                     n_components = params['n_components'], 
+                                                     min_cluster_size = params['min_cluster_size'],
+                                                     random_state = params['random_state'])
+    
+    label_count, cost = score_clusters(clusters, prob_threshold = 0.05)
+    
+    #15% penalty on the cost function if outside the desired range of groups
+    if (label_count < label_lower) | (label_count > label_upper):
+        penalty = 0.15 
+    else:
+        penalty = 0
+    
+    loss = cost + penalty
+    
+    return {'loss': loss, 'label_count': label_count, 'status': STATUS_OK}
+
+def bayesian_search(embeddings, space, label_lower, label_upper, max_evals=100):
+    """
+    Perform bayseian search on hyperopt hyperparameter space to minimize objective function
+    """
+    
+    trials = Trials()
+    fmin_objective = partial(objective, embeddings=embeddings, label_lower=label_lower, label_upper=label_upper)
+    best = fmin(fmin_objective, 
+                space = space, 
+                algo=tpe.suggest,
+                max_evals=max_evals, 
+                trials=trials)
+
+    best_params = space_eval(space, best)
+    print ('best:')
+    print (best_params)
+    print (f"label count: {trials.best_trial['result']['label_count']}")
+    
+    best_clusters, best_reduced_embeddings = generate_clusters(embeddings, 
+                                                              n_neighbors = best_params['n_neighbors'], 
+                                                              n_components = best_params['n_components'], 
+                                                              min_cluster_size = best_params['min_cluster_size'],
+                                                              random_state = best_params['random_state'])
+    
+    return best_params, best_clusters, best_reduced_embeddings, trials
